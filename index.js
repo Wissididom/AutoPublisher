1554f0c1266f21f16c581053ca2ffc0483a86d45
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-04-26 12:53:04 +0000

    scripts, prettier and dependabot

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits, Partials, ChannelType } = require('discord.js');
-const mySecret = process.env['TOKEN'];  // Discord Token
-const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent], partials: [Partials.User, Partials.Channel, Partials.GuildMember, Partials.Message, Partials.Reaction]}); // Discord Object
+require("dotenv").config();
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  ChannelType,
+} = require("discord.js");
+const mySecret = process.env["TOKEN"]; // Discord Token
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  partials: [
+    Partials.User,
+    Partials.Channel,
+    Partials.GuildMember,
+    Partials.Message,
+    Partials.Reaction,
+  ],
+}); // Discord Object
 
 client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("messageCreate", message => {
-	//console.log(`${message.content}: ${message.channel.type}`);
-	if (message.channel.type == ChannelType.GuildNews || message.channel.type == ChannelType.GuildNewsThread) {
-		message.crosspost().then(() => console.log('Crossposted message')).catch(console.error);
-	}
+client.on("messageCreate", (message) => {
+  //console.log(`${message.content}: ${message.channel.type}`);
+  if (
+    message.channel.type == ChannelType.GuildNews ||
+    message.channel.type == ChannelType.GuildNewsThread
+  ) {
+    message
+      .crosspost()
+      .then(() => console.log("Crossposted message"))
+      .catch(console.error);
+  }
 });
 
 // Bot Login
 if (!mySecret) {
-  console.log("TOKEN not found! You must setup the Discord TOKEN as per the README file before running this bot.");
+  console.log(
+    "TOKEN not found! You must setup the Discord TOKEN as per the README file before running this bot.",
+  );
 } else {
   client.login(mySecret);
 }
